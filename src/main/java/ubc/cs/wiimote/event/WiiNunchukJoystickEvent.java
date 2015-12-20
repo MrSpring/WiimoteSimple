86987d72b91acf5b6bb97496e979c72299836c54
MrSpring <mrspring@mrspring.dk>
2015-12-20 23:39:02 +0100

    Added WiiNunchukJoystickEvent, since it was missing(?) moved sources to src/main/java

---
+++ src/main/java/ubc/cs/wiimote/event/WiiNunchukJoystickEvent.java
@@ -0,0 +1,18 @@
+package ubc.cs.wiimote.event;
+
+import ubc.cs.wiimote.Wiimote;
+
+/**
+ * Created on 20-12-2015 for WiimoteSimple.
+ */
+public class WiiNunchukJoystickEvent extends WiiEvent
+{
+    final int x, y;
+
+    public WiiNunchukJoystickEvent(Wiimote wiimote, int x, int y)
+    {
+        super(wiimote);
+        this.x = x;
+        this.y = y;
+    }
+}
